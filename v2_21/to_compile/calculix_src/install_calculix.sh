9cffc3036f43add15684af17490be6f10e016457
giuseppe <giuseppe.abbiati@gmail.com>
2024-05-15 15:28:38 +0200

    calculix installation script drafted

--- v2_21/to_compile/calculix_src/install_calculix.sh
+++ v2_21/to_compile/calculix_src/install_calculix.sh
@@ -1,23 +1,49 @@
+#!/bin/bash
+
+# exit script on error
+set -e
+
+# check if the executable is already available
+if [ -f "/usr/local/bin/ccx_2.21"]; then
+	echo "Calculix 2.21 is already installed"
+	exit 0
+else
+	echo "Calculix 2.21 compiling starts"
+fi
+
+# go to calculix source code folder
+cd /calculix_src/
+
+# decompress archives for calculix source code
 bzip2 -dk ccx_2.21.src.tar.bz2
 tar -xvf ccx_2.21.src.tar
 
+# decompress archives for spools source code
 mkdir spooles.2.2
 tar -xzvf spooles.2.2.tgz -C spooles.2.2
+
+# decompress archive for arpack source code
 unzip -l chaoyang2013-arpack-4b070f981380
 
+# replace make files with the customized ones
 cp -fv Make.inc_ spooles.2.2/Make.inc
 cp -fv ARmake.inc_ chaoyang2013-arpack-4b070f981380/ARmake.inc
 cp -fv makefile_ CalculiX/ccx_2.21/src/makefile
 
-cd(/calculix_src/spooles.2.2/)
+# compile spools
+cd /calculix_src/spooles.2.2/
 make lib
 
-cd(/calculix_src/chaoyang2013-arpack-4b070f981380/)
+# compile arpak
+cd /calculix_src/chaoyang2013-arpack-4b070f981380/
 make lib
 
-cd(/calculix_src/CalculiX/ccx_2.21/src/)
+# compile calculix
+cd /calculix_src/CalculiX/ccx_2.21/src/
 make
 
+# move calculix executable into bin folder
 mv ccx_2.21 /usr/local/bin
 
-cd(/calculix_run/)
+# move to the run folder
+cd /calculix_run/
